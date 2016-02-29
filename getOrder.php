7ff33e175379f5ca13cc0bd0551663d82b8f9882
Martin LaBrecque <martinl@finelink.com>
2016-02-29 16:58:54 -0500

    Colab form Phase 1

--- getOrder.php
+++ getOrder.php
@@ -1,6 +1,6 @@
 <?php
  
- $proxy = new SoapClient('http://magento.finelink.com/api/v2_soap/?wsdl', array('trace' => 1)); 
+$proxy = new SoapClient('http://magento.finelink.com/api/v2_soap/?wsdl', array('trace' => 1)); 
 $sessionId = $proxy->login('soaper', 'F!neline25'); 
 
 $result = $proxy->salesOrderInvoiceList($sessionId);
